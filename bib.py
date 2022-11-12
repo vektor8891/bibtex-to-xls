19ed05f1d48826e282332838b78941b3ded4e18c
vektor8891 <vszabo@protonmail.com>
2022-11-12 09:09:01 -0500

    specify engine

--- bib.py
+++ bib.py
@@ -8,4 +8,4 @@ with open("../../Dropbox/Backup/konyvek.bib") as bibtex_file:
 
 df = pd.DataFrame(bib_database.entries)
 selection = df[['author', 'title', 'year', 'isbn', 'url']]
-selection.to_excel("../../Dropbox/Backup/konyvek.xls", index=False)
+selection.to_excel("../../Dropbox/Backup/konyvek.xls", index=False, engine='xlsxwriter')
