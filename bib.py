8f60f10d2244d4a0bae937b96f3c2b5de29b2ce5
vektor8891 <vszabo@protonmail.com>
2022-11-12 09:14:23 -0500

    sort books

--- bib.py
+++ bib.py
@@ -7,5 +7,6 @@ with open("../../Dropbox/Backup/konyvek.bib") as bibtex_file:
     bib_database = bibtexparser.load(bibtex_file)
 
 df = pd.DataFrame(bib_database.entries)
-selection = df[['author', 'title', 'year', 'isbn', 'url']]
-selection.to_excel("../../Dropbox/Backup/konyvek.xls", index=False, engine='xlsxwriter')
+books = df[['author', 'title', 'year', 'isbn', 'url']]
+books_sorted = books.sort_values(by=['author', 'title'])
+books_sorted.to_excel("../../Dropbox/Backup/konyvek.xls", index=False, engine='xlsxwriter')
